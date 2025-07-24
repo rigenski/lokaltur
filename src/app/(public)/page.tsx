594c584814f562b6b1642ef536b71a823de9c2e1
Rigen Maulana <rygen.dev@gmail.com>
2025-07-24 22:09:07 +0700

    fix: change menu on page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,12 +1,5 @@
-import { Button } from "@/components/ui/button";
+import Container from "./_components/container";
 
 export default function Home() {
-  return (
-    <section className="w-full">
-      <div className="mx-auto w-[90%] px-4 py-8">
-        <h1 className="font-gaeilge-kids">LOKALTUR</h1>
-        <Button>The choice of a light cream</Button>
-      </div>
-    </section>
-  );
+  return <Container />;
 }
